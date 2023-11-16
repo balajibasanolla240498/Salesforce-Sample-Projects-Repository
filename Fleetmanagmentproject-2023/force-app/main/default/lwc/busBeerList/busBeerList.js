6ad0962380f3847fe7da7333ed451df5ac60d49f
Balaji B <balu24041998@gmail.com>
2023-11-16 18:47:09 +0530

    updated

--- Fleetmanagmentproject-2023/force-app/main/default/lwc/busBeerList/busBeerList.js
+++ Fleetmanagmentproject-2023/force-app/main/default/lwc/busBeerList/busBeerList.js
@@ -3,6 +3,9 @@ import getBusData from '@salesforce/apex/LWCController.getBusData';
 
 export default class BusBeerList extends LightningElement 
 {
+    /* 
+    addes some text
+    */
     //wire decorator to get data
     @wire(getBusData)
     buses;
